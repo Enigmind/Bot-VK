2fa90ecdfad7aa326980b58d62fc7afb99cfbad7
Yann ANTONINI <darjador280@gmail.com>
2020-09-10 11:18:54 +0200

    update conf.json

--- anick.js
+++ anick.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js')
 const fs = require('fs')
 const Welcome = require("discord-welcome");
-const bot_token = require('./auth.json').token;
-const {prefix} = require('./config.json');
+const {prefix, token} = require('./config.json');
 
 const client = new Discord.Client()
 client.commands = new Discord.Collection();
@@ -18,14 +17,6 @@ for (const file of commandFiles) {
 //when someone slide into the server
 Welcome(client, {
   "526207992182472704": {
-    privatemsg: "Bonjour et bienvenue chez Veritas Kingdom !!\n"
-      + "Je suis Anick, l'intelligence artificielle de l'escadron. Laisse moi t'expliquer quelques trucs avant de débuter chez nous.\n"
-      + "Nous sommes un escadron chill et sans prise de tête. Nous jouons pour le RP et les interactions entre joueurs. Il n'y a aucune obligation de connexion ni de temps de jeu.\n"
-      + "Si tu viens pour faire partie de l'Union, je t'invite à contacter Gharkan\n"
-      + "Si tu es juste là en tant qu'invité, bienvenue sur notre discord, il te sera demandé de respecter les règles au sein de celui-ci afin que tout se passe pour le mieux\n"
-      + "Si tu veux rejoindre notre escadron In Game, il te faudra faire une candidature dans le jeu (id : 3301) qu'un amiral validera. une fois ceci fait, tu obtiendra l'accès complet au discord au sein duquel il te sera demandé de respecter les règles (Rien de bien méchant, il s'agit principalement des règles de base de bienséance ;)\n"
-      + "N'hésite pas à faire appel à un membre si tu as besoin d'aide ou d'informations complémentaires. Tu peux également me solliciter avec le préfix : `§` (tape `§help` pour savoir dans quelles mesures je peux me rendre utile)\n"
-      + "Bon vol Commandant, et à bientôt dans l'escadron je l'espère :kissing_heart: !",
     publicmsg: "Bonjour @MEMBER et bienvenue chez Veritas Kingdom !!"
       + "\nN'hésite pas à rejoindre l'escadron sur Elite (ID=3301), en suivant les informations fournies dans #accueil ou bien à demander de plus amples informations aux membres de l'escadron ici même."
       + "\n**VERITAS VINCIT !** ",
@@ -75,4 +66,4 @@ client.on('message', (message) => {
   }
 })
 
-client.login(bot_token)
+client.login(token)
